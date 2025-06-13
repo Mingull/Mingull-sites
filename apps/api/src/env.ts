8ab7a2878a49f4252cf0f811adf27b09b9786a16
Mingull <60094450+Mingull@users.noreply.github.com>
2025-06-13 18:43:24 +0200

    Update env.ts

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -2,8 +2,9 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { config } from "dotenv";
 import { z } from "zod";
 
-config({ path: "../../.env" });
-
+if (process.env.NODE_ENV === "development") {
+  config({ path: "../../.env" });
+}
 export const env = createEnv({
 	server: {
 		BASE_API: z.string().url(),
