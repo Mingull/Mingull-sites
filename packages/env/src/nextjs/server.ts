9eaa3353266ae1f491d5487a035a19195ae0253b
Mingull <nielsplug@outlook.com>
2025-07-09 22:57:29 +0200

    removed 3d-card because no use

--- packages/env/src/nextjs/server.ts
+++ packages/env/src/nextjs/server.ts
@@ -3,9 +3,7 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { config } from "dotenv";
 import { z } from "zod";
 
-// const envConfig = process.env.NODE_ENV !== "production" ? config({ path: "../../.env" }) : config();
-
-console.log({ env: process });
+config({ path: "../../.env" });
 
 export const env = createEnv({
 	shared: {
