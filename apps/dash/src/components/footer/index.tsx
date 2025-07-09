a02b2bba1bfae9a75eba77dc8eac3f7b2eed1271
Mingull <nielsplug@outlook.com>
2025-07-09 21:10:18 +0200

    fixed dash footer

--- apps/dash/src/components/footer/index.tsx
+++ apps/dash/src/components/footer/index.tsx
@@ -1,49 +1,135 @@
-import { navigation } from "./navigation"; // your config
+"use client";
+
+import { cn } from "@mingull/lib";
+import { Badge } from "@mingull/ui/comps/badge";
+import { useNavigation } from "./navigation";
+import { useId } from "react";
 
 export default function Footer() {
+	const navigation = useNavigation();
+	const id = useId();
+
+	if (!navigation) return null;
+
+	const isHorizontal = navigation.orientation === "horizontal";
+
 	return (
-		<footer className="bg-muted py-16">
-			<div className="container max-w-3xl">
-				<div className="md:flex md:items-center md:justify-between">
-					<div className="flex w-full justify-between gap-4">
-						{navigation.groups?.map((group) => {
+		<footer
+			className={cn("bg-muted text-muted-foreground text-sm", {
+				"py-16": isHorizontal,
+				"py-8": !isHorizontal,
+			})}
+		>
+			<div className="container mx-0 max-w-3xl px-4 sm:mx-auto">
+				<div
+					className={cn("flex w-full", {
+						"flex-row items-start justify-between gap-12": isHorizontal,
+						"flex-col items-center gap-6 text-center": !isHorizontal,
+					})}
+				>
+					{/* Title Section */}
+					<p
+						className={cn(
+							"text-foreground flex w-full items-center gap-2 font-serif text-2xl font-semibold",
+							{
+								"justify-center": !isHorizontal,
+							},
+						)}
+					>
+						{navigation.icon && <navigation.icon className="text-primary h-12 w-12" />}
+						<span>{navigation.title}</span>
+					</p>
+
+					{/* Group Navigation Section */}
+					<div
+						className={cn("flex w-full", {
+							"flex-col items-center gap-8": !isHorizontal,
+							"flex-row justify-end gap-12": isHorizontal,
+						})}
+					>
+						{navigation.groups?.map((group, key) => {
 							const GroupIcon = group.icon;
-							return (
-								<div key={group.label}>
-									<div className="text-muted-foreground flex items-center gap-2 font-semibold">
-										{GroupIcon ?
-											<GroupIcon className="h-4 w-4" />
-										:	null}
+							const isGroupHorizontal = group.orientation === "horizontal";
+							const groupKey = `${id}-${group.label ?? key}`;
+
+							const showLabel = group.label && !group.header;
+							const showHeader = group.header && !group.label;
+
+							const labelContent =
+								showLabel ?
+									<p className="mb-2 flex items-center justify-start gap-2 font-semibold">
+										{GroupIcon && <GroupIcon className="h-4 w-4" />}
 										{group.label}
-									</div>
-									<ul className="mt-2 space-y-1">
+									</p>
+								: showHeader ?
+									<p className="mb-2 min-w-fit font-semibold">
+										{typeof group.header === "function" ?
+											group.header(new Date().getFullYear())
+										:	group.header}
+									</p>
+								:	null;
+
+							const footerContent =
+								group.footer ?
+									<p className="mb-2 min-w-fit font-semibold">
+										{typeof group.footer === "function" ? group.footer() : group.footer}
+									</p>
+								:	null;
+
+							return (
+								<div
+									key={groupKey}
+									className={cn("text-foreground", {
+										"text-center": !isGroupHorizontal,
+										"text-muted-foreground": group.muted,
+										"sm:gap-22 flex w-full items-center justify-center": isGroupHorizontal,
+										"justify-evenly": group.header,
+									})}
+								>
+									{labelContent}
+
+									<ul
+										className={cn("space-y-1", {
+											"flex w-full flex-wrap justify-center space-x-1 space-y-0 text-base sm:space-x-6":
+												isGroupHorizontal,
+											"justify-evenly": !group.header,
+										})}
+									>
 										{group.items.map((item) => {
 											const ItemIcon = item.icon;
+
 											return (
 												<li
 													key={item.label}
-													className={item.disabled ? "cursor-not-allowed opacity-50" : ""}
+													className={cn({
+														"cursor-not-allowed opacity-50": item.disabled,
+													})}
 												>
 													<a
 														href={item.href}
 														target={item.external ? "_blank" : undefined}
 														rel={item.external ? "noopener noreferrer" : undefined}
-														className="flex items-center gap-2 text-sm hover:underline"
+														className={cn("flex items-center gap-2 hover:underline", {
+															"justify-center gap-6": isGroupHorizontal,
+														})}
 													>
-														{ItemIcon ?
-															<ItemIcon className="h-4 w-4" />
-														:	null}
-														{item.label}
-														{item.badge ?
-															<span className="bg-muted ml-auto rounded px-2 py-0.5 text-xs">
-																{item.badge}
-															</span>
-														:	null}
+														{ItemIcon && (
+															<ItemIcon
+																className={cn("h-4 w-4", {
+																	"mr-2 h-6 w-6": isGroupHorizontal,
+																	"text-muted-foreground": group.muted,
+																})}
+															/>
+														)}
+														{(group.orientation === "vertical" || !item.iconOnly) &&
+															item.label}
+														{item.badge && <Badge>{item.badge}</Badge>}
 													</a>
 												</li>
 											);
 										})}
 									</ul>
+									{footerContent}
 								</div>
 							);
 						})}
--- apps/dash/src/components/footer/navigation.tsx
+++ apps/dash/src/components/footer/navigation.tsx
@@ -1,68 +1,73 @@
-import { DiscordIcon, GitHubIcon } from "@mingull/icons";
+"use client";
+import { DiscordIcon, GitHubIcon, MingullIcon } from "@mingull/icons";
 import { LinkedInLogoIcon } from "@radix-ui/react-icons";
-import { BookUserIcon, CompassIcon, Globe2Icon } from "lucide-react";
 import { Navigation } from "./type";
 
-export const navigation: Navigation = {
-	groups: [
-		{
-			label: "Explore",
-			icon: CompassIcon,
-			items: [
-				{
-					label: "Home",
-					href: "/",
-				},
-				{
-					label: "Projects",
-					href: "/projects",
-				},
-				{
-					label: "Posts",
-					href: "/posts",
-				},
-				{
-					label: "Contact",
-					href: "/contact",
-				},
-			],
-		},
-		{
-			label: "Social",
-			icon: Globe2Icon,
-			items: [
-				{
-					label: "GitHub",
-					href: "https://github.com/gebruikersnaam",
-					description: "View my open source work",
-					icon: GitHubIcon,
-					external: true,
-				},
-				{
-					label: "LinkedIn",
-					href: "https://linkedin.com/in/gebruikersnaam",
-					description: "Connect with me on LinkedIn",
-					icon: LinkedInLogoIcon,
-					external: true,
-				},
-				{
-					label: "Discord",
-					href: "https://discord.gg/jouwserver",
-					description: "Join my Discord community",
-					icon: DiscordIcon,
-					external: true,
-				},
-			],
-		},
-		{
-			label: "Contact",
-			icon: BookUserIcon,
-			items: [
-				{
-					label: "example@example.com",
-					href: "mailto:example@example.com",
-				},
-			],
-		},
-	],
+export const useNavigation = (): Navigation => {
+	return {
+		title: "Mingull",
+		icon: MingullIcon,
+		orientation: "vertical",
+		groups: [
+			{
+				header: "",
+				footer: "",
+				orientation: "horizontal",
+				items: [
+					{
+						label: "Home",
+						href: "/",
+					},
+					{
+						label: "Projects",
+						href: "/projects",
+					},
+					{
+						label: "Posts",
+						href: "/posts",
+					},
+					{
+						label: "Contact",
+						href: "/contact",
+					},
+				],
+			},
+			{
+				orientation: "horizontal",
+				header: (year) => <>&copy; {year} Mingull</>,
+				footer: (
+					<>
+						Built by <span className="font-bold">Mingull</span>
+					</>
+				),
+				muted: true,
+				items: [
+					{
+						label: "GitHub",
+						href: "https://github.com/gebruikersnaam",
+						description: "View my open source work",
+						icon: GitHubIcon,
+						external: true,
+						iconOnly: true,
+					},
+					{
+						label: "LinkedIn",
+						href: "https://linkedin.com/in/gebruikersnaam",
+						description: "Connect with me on LinkedIn",
+						icon: LinkedInLogoIcon,
+						external: true,
+						iconOnly: true,
+					},
+					{
+						label: "Discord",
+						href: "https://discord.gg/jouwserver",
+						description: "Join my Discord community",
+						icon: DiscordIcon,
+						external: true,
+						iconOnly: true,
+					},
+				],
+			},
+		],
+	};
 };
